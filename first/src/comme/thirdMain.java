9ff68101a54217c338fa58c27f1fd2bcd0844304
FAN <136819053@qq.com>
2022-08-29 16:22:01 +0800

    新增了thirdMain类

---
+++ first/src/comme/thirdMain.java
@@ -0,0 +1,11 @@
+package comme;
+
+public class thirdMain {
+    public static void main(String[] args) {
+        System.out.println("third--helloExp!!!");
+        System.out.println("third--helloExp!!");
+        System.out.println("third--helloExp!");
+        System.out.println("third--helloExp");
+
+    }
+}
