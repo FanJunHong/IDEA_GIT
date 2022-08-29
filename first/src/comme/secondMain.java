cc7b9cbab3b82a0fc33f4695b4174ec5d3c456f6
FAN <136819053@qq.com>
2022-08-29 16:07:15 +0800

    新增了secondMain类

---
+++ first/src/comme/secondMain.java
@@ -0,0 +1,11 @@
+package comme;
+
+public class secondMain {
+    public static void main(String[] args) {
+        System.out.println("second--helloExp!!!");
+        System.out.println("second--helloExp!!");
+        System.out.println("second--helloExp!");
+        System.out.println("second--helloExp");
+
+    }
+}
