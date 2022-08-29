82d173c383ea4b85d253da9f82a44954520e9948
FAN <136819053@qq.com>
2022-08-29 16:18:34 +0800

    修改了firstMain类

--- first/src/comme/firstMain.java
+++ first/src/comme/firstMain.java
@@ -2,10 +2,10 @@ package comme;
 
 public class firstMain {
     public static void main(String[] args) {
-        System.out.println("helloExp!!!");
-        System.out.println("helloExp!!");
-        System.out.println("helloExp!");
-        System.out.println("helloExp");
-
+            System.out.println("helloExp!!!");
+            System.out.println("helloExp!!");
+            System.out.println("helloExp!");
+            System.out.println("helloExp");
+            System.out.println("修改了文件，但是没有commit，文件名会变成蓝色");
     }
 }
