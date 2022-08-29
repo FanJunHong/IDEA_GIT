ce6f1738856513c313d4361611a15100e18e6827
FAN <136819053@qq.com>
2022-08-29 15:58:55 +0800

    修改了firstMain的代码

--- first/src/comme/firstMain.java
+++ first/src/comme/firstMain.java
@@ -3,5 +3,9 @@ package comme;
 public class firstMain {
     public static void main(String[] args) {
         System.out.println("helloExp!!!");
+        System.out.println("helloExp!!");
+        System.out.println("helloExp!");
+
+
     }
 }
