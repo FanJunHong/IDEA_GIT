7ac00ef8114d4dba6c5cd283a58aa27eb9c260d5
FAN <136819053@qq.com>
2022-09-05 14:32:26 +0800

    新增了test包

--- first/src/testpackage/testEnum.java
+++ first/src/testpackage/testEnum.java
@@ -4,14 +4,26 @@ import java.util.Scanner;
 
 public class testEnum {
     public static void main(String[] args) {
+        //获取枚举中的某个量
+        Size se = Size.SMALL;
+        System.out.println("枚举中的量："+se);
+
+        //遍历枚举中的所有量
+        System.out.println("枚举中的所有数据为：");
+        for(Size se1: Size.values())
+            System.out.print(se1+"  ");
+        System.out.println(" ");
+
+        //测试枚举中是否存在所输入的量
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a size:(SMALL,MEDIUM,LARGE,EXTRA_LAGRE");
         String input = in.next().toUpperCase();
-        Size size = Enum.valueOf(Size.class, input);
+        Size size = Enum.valueOf(Size.class, input);  //valueOf()方法返回指定字符串的枚举常量
         System.out.println("size="+size);
         System.out.println("abbr="+size.getAbbr());
         if (size == size.EXTRA_LARGE)
             System.out.println("Good job");
+
     }
 }
 
