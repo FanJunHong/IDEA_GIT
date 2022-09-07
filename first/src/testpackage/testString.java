72ccd6b4bb3901e0a7d2e36c7c5c09ac9e2c5b53
FAN <136819053@qq.com>
2022-09-07 18:52:11 +0800

    更新了testString

--- first/src/testpackage/testString.java
+++ first/src/testpackage/testString.java
@@ -14,6 +14,14 @@ public class testString {
         Integer a = 123;
         System.out.println(a.toString());
 
+        String str2 = "QWERTYUIOP";
+        String str3 = "qwertyuiop";
+        if (str2.equals(str3))       //会区分大小写
+            System.out.println("str2.equals(str3) 成立");
+        else
+            System.out.println("str2.equals(str3) 不成立");
 
+        if (str2.equalsIgnoreCase(str3))   //会忽略大小写
+            System.out.println("str2.equalsIgnoreCase(str3)成立");
     }
 }
